3487f8e403fd11be19e24a4938c3836c7551a63c
Stéphane Brunner <stephane.brunner@camptocamp.com>
2015-06-05 10:04:32 +0200

    Start the version 1.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = [
 
 setup(
     name='c2c.versions',
-    version='1.0.0',
+    version='1.1.0',
     description='Command to tests program version',
     long_description=README,
     namespace_packages=['c2c'],
