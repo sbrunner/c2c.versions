61d83da8bb70e3318b3acbebd737e4c028fc6d49
Stéphane Brunner <stephane.brunner@camptocamp.com>
2016-03-24 09:05:32 +0100

    Do the release 1.0.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = [
 
 setup(
     name='c2c.versions',
-    version='1.1.0',
+    version='1.0.1',
     description='Command to tests program version',
     long_description=README,
     namespace_packages=['c2c'],
